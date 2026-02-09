5ac86552652766e9423ee599cc5755a934b98ac2
nelakurtirahul <nelakurtirahul@gmail.com>
2026-02-09 15:23:43 -0500

    Student Details

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Student s1 = new Student(1, "Rahul", "ECE", 8.5);
-        System.out.println(s1);
+        StudentService service = new StudentService();
+        service.addStudent(new Student(1, "Rahul", "ECE",8.5));
+        service.addStudent(new Student(2, "Ankit", "CSE", 8.1));
+        service.displayStudents();
     }
 }
---
+++ StudentService.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+public class StudentService {
+    private List<Student> students = new ArratList<>();
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student added successfully.");
+    }
+    public void displayStudents() {
+        if(students.isEmpty()) {
+            System.out.println("No students found.");
+            return;
+        }
+        for(Student s : students) {
+            System.out.println(s);
+        }
+    }
+}
