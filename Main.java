05c8151fced985dec81a06b343ec1fe8b45d7def
nelakurtirahul <nelakurtirahul@gmail.com>
2026-02-09 16:07:31 -0500

    CRUD Update

--- Main.java
+++ Main.java
@@ -3,6 +3,14 @@ public class Main{
         StudentService service = new StudentService();
         service.addStudent(new Student(1, "Rahul", "ECE",8.5));
         service.addStudent(new Student(2, "Ankit", "CSE", 8.1));
+        System.out.println("All Students:");
+        service.displayStudents();
+        System.out.println("\nSearching for student with ID 1:");
+        Student found = service.getStudentId(1);
+        System.out.println(found != null ? found : "Student not found");
+        System.out.println("\nDeleting student with ID 2:");
+        service.deleteStudentByID(2);
+        System.out.println("\nAll student after deletion:");
         service.displayStudents();
     }
 }
--- StudentService.java
+++ StudentService.java
@@ -1,18 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 public class StudentService {
-    private List<Student> students = new ArrayList<>();
+    private Map<Integer, Student> studentMap = new HashMap<>();
     public void addStudent(Student student) {
-        students.add(student);
-        System.out.println("Student added successfully.");
+        studentMap.put(student.getId(), student);
+        System.out.println("Student added");
     }
     public void displayStudents() {
-        if(students.isEmpty()) {
-            System.out.println("No students found.");
+        if(studentMap.isEmpty()) {
+            System.out.println("No Student");
             return;
         }
-        for(Student s : students) {
+        for(Student s: studentMap.values()) {
             System.out.println(s);
+        }
+    }
+    public Student getStudentId(int id) {
+        return studentMap.get(id);
+    }
+    public void deleteStudentByID(int id) {
+        if(studentMap.containsKey(id)) {
+            studentMap.remove(id);
+            System.out.println("Student removed");
+        } else {
+            System.out.println("Student not found.");
         }
     }
 }
